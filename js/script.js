d17f927c9bacda1a730eddcd546a3cebab923295
saikiran03 <kiransai3284462@gmail.com>
2016-03-04 16:26:48 +0530

    completed single step methods

--- js/script.js
+++ js/script.js
@@ -14,13 +14,21 @@ function evaluate(x, y, xpoly, ypoly){
 	return res;
 }
 
-function bem_iterator(x,y,f,h,xpoly,ypoly){
-	var tmp = y;
-	y = y + h*evaluate(x, y, xpoly, ypoly);
-	if(y-tmp>0.01){
-		return bem_iterator(x,y,f,h,xpoly,ypoly);
+function bem_iterator(x, y, yk, h, xpoly, ypoly){
+	var yn = y + h*evaluate(x, yk, xpoly, ypoly);
+	if(Math.abs(yn-yk) > 0.01){
+		return bem_iterator(x, y, yn, h, xpoly, ypoly);
 	}else{
-		return y;
+		return yn;
+	}
+}
+
+function mem_iterator(x, y, f, h, yk, xpoly, ypoly){
+	var yn = y + h*(f + evaluate(x+h, yk, xpoly, ypoly))*0.5;
+	if(Math.abs(yn-yk) > 0.01){
+		return mem_iterator(x, y, f, h, yn, xpoly, ypoly);
+	}else{
+		return yn;
 	}
 }
 
@@ -29,7 +37,7 @@ function bem_iterator(x,y,f,h,xpoly,ypoly){
 function forward_euler_method(a, b, h, y, xpoly, ypoly){
 	var yi,f,n = (b-a)/h;
 	var results = [y];
-	for(var i=0; i<=n; i++){
+	for(var i=0; i<n; i++){
 		f = evaluate(a+(i*h), results[i], xpoly, ypoly);
 		yi = Math.round((results[i] + (h*f)) * multiplier)/multiplier;
 		results.push(yi);
@@ -38,48 +46,67 @@ function forward_euler_method(a, b, h, y, xpoly, ypoly){
 }
 
 function backward_euler_method(a, b, h, y, xpoly, ypoly){
-	var py, yi, f, n = (b-a)/h;
+	var yi, n = (b-a)/h;
 	var results = [y];
-	for(var i=0; i<=n; i++){
-		f = evaluate(a+((i+1)*h), results[i], xpoly, ypoly);
-		yi = results[i] + (h*f);
-		py = results[i];
-		console.log(yi);
-		while(yi-py > 0.01){
-			py = yi;
-			f = evaluate(a+((i+1)*h), py, xpoly, ypoly)
-			yi = results[i] + h*f;
-			console.log(yi);
-		}
+	for(var i=0; i<n; i++){
+		yi = bem_iterator(a+(i+1)*h, results[i], results[i], h, xpoly, ypoly);
 		yi = Math.round(yi*multiplier)/multiplier;
 		results.push(yi);
 	}
 	return results;
 }
 
-function modified_euler_method(){
-
+function modified_euler_method(a, b, h, y, xpoly, ypoly){
+	var tmpy, n = (b-a)/h;
+	var results = [y];
+	for(var i=0; i<n; i++){
+		tmpy = mem_iterator(a+);
+		results.push(Math.round(tmpy*multiplier)*multiplier);
+	}
+	return results;
 }
 
-function classical_rk_method(){
+function euler_cauchy_method(a, b, h, y, xpoly, ypoly){
+	var tmpy, k1, k2, n = (b-a)/h;
+	var results = [y];
+	for( var i=0; i<n; i++){
+		k1 = h * evaluate(a+(h*i), results[i], xpoly, ypoly);
+		k2 = h * evaluate(a+(i+0.5)*h, results[i] + k1/2, xpoly, ypoly);
+		tmpy = results[i] + (k1+k2)/2;
+		results.push(Math.round(tmpy*multiplier)/multiplier);
+	}
+	return results;
+}
 
+function classical_rk_method(a, b, h, y, xpoly, ypoly){
+	var tmpy, k1, k2, k3, k4, n = (b-a)/h;
+	var results = [y];
+	for(var i=0; i<n; i++){
+		k1 = h * evaluate(a+(i*h), results[i], xpoly, ypoly);
+		k2 = h * evaluate(a+(i+0.5)*h, results[i]+k1/2, xpoly, ypoly);
+		k3 = h * evaluate(a+(i+0.5)*h, results[i]+k2/2, xpoly, ypoly);
+		k4 = h * evaluate(a+(i+1)*h, results[i]+k3, xpoly, ypoly);
+		tmpy = results[i] + (k1 + k2 + k3 + k4)/6;
+		results.push(Math.round(tmpy*multiplier)/multiplier);
+	}
+	return results;
 }
 
 //MultiStep methods.
 
-function nystrom_central_difference_method(){
+function nystrom_central_difference_method(a, b, h, y, xpoly, ypoly){
 
 }
 
-function adam_bashforth_method(){
+function adam_bashforth_method(a, b, h, y, xpoly, ypoly){
 
 }
 
-function adam_moutron_method(){
+function adam_moutron_method(a, b, h, y, xpoly, ypoly){
 
 }
 
-function milne_simpson_method(){
+function milne_simpson_method(a, b, h, y, xpoly, ypoly){
 
 }
 
