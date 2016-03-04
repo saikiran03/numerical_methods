72cd9c14cdcd9c78aca5329020f4bdf7acd129d4
saikiran03 <kiransai3284462@gmail.com>
2016-03-04 17:40:42 +0530

    added second order implicit rk method

--- js/script.js
+++ js/script.js
@@ -32,6 +32,16 @@ function mem_iterator(x, y, f, h, yk, xpoly, ypoly){
 	}
 }
 
+function implicit_rk_iterator(x, y, h, kn, xpoly, ypoly){
+	var k;
+	k = evaluate(x + h*0.5, y + (h*kn*0.5), xpoly, ypoly);
+	if(Math.abs(kn-k) > 0.01){
+		return implicit_rk_iterator(x, y, h, k, xpoly, ypoly);
+	}else{
+		return k;
+	}
+}
+
 //Single Step methods.
 
 function forward_euler_method(a, b, h, y, xpoly, ypoly){
@@ -60,7 +70,7 @@ function modified_euler_method(a, b, h, y, xpoly, ypoly){
 	var tmpy, n = (b-a)/h;
 	var results = [y];
 	for(var i=0; i<n; i++){
-		tmpy = mem_iterator(a+);
+		tmpy = mem_iterator(a+(i*h), results[i], evaluate(a+(i*h), results[i]), h, results[i], xpoly, ypoly);
 		results.push(Math.round(tmpy*multiplier)*multiplier);
 	}
 	return results;
@@ -78,6 +88,17 @@ function euler_cauchy_method(a, b, h, y, xpoly, ypoly){
 	return results;
 }
 
+function second_order_rk_method(a, b, h, y, xpoly, ypoly){
+	var results = [y];
+	var k1, tmpy, n=(b-a)/h;
+	for(var i=0; i<n; i++){
+		k1 = implicit_rk_iterator(a+(i*h), results[i], h, results[i], xpoly, ypoly);
+		tmpy = results[i] + h*k1;
+		results.push(Math.round(tmpy*multiplier)/multiplier);
+	}
+	return results;
+}
+
 function classical_rk_method(a, b, h, y, xpoly, ypoly){
 	var tmpy, k1, k2, k3, k4, n = (b-a)/h;
 	var results = [y];
@@ -86,7 +107,7 @@ function classical_rk_method(a, b, h, y, xpoly, ypoly){
 		k2 = h * evaluate(a+(i+0.5)*h, results[i]+k1/2, xpoly, ypoly);
 		k3 = h * evaluate(a+(i+0.5)*h, results[i]+k2/2, xpoly, ypoly);
 		k4 = h * evaluate(a+(i+1)*h, results[i]+k3, xpoly, ypoly);
-		tmpy = results[i] + (k1 + k2 + k3 + k4)/6;
+		tmpy = results[i] + (k1 + 2*k2 + 2*k3 + k4)/6;
 		results.push(Math.round(tmpy*multiplier)/multiplier);
 	}
 	return results;
@@ -113,3 +134,5 @@ function milne_simpson_method(a, b, h, y, xpoly, ypoly){
 function milne_method(){
 
 }
+
+//functions to render output to display
